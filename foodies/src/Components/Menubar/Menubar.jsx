2b11238430c51de60ebf8a288d213df218ab197a
Yash Panchal <panchalyash089@gmail.com>
2025-12-29 17:04:37 +0530

    added cart logo

--- foodies/src/Components/Menubar/Menubar.jsx
+++ foodies/src/Components/Menubar/Menubar.jsx
@@ -13,30 +13,22 @@ const Menubar = () => {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">Home</a>
+                            <a className="nav-link"  href="#">Home</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Link</a>
-                        </li>
-                        <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Dropdown
-                            </a>
-                            <ul className="dropdown-menu">
-                                <li><a className="dropdown-item" href="#">Action</a></li>
-                                <li><a className="dropdown-item" href="#">Another action</a></li>
-                                <li><hr className="dropdown-divider" /></li>
-                                <li><a className="dropdown-item" href="#">Something else here</a></li>
-                            </ul>
+                            <a className="nav-link" href="#">Explore</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+                            <a className="nav-link" href="#">Contact us</a>
                         </li>
                     </ul>
-                    <form className="d-flex" role="search">
-                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                        <button className="btn btn-outline-success" type="submit">Search</button>
-                    </form>
+                    <div className="menubar-right">
+                        <div className="position-relative">
+                            <img src={assets.cart} alt="" height={32} width={32} className='position-relative'/>
+                            <span className='position-absolute top-0 start-100 translate-middle badge rounded-pill bg-warning'>5</span>
+                        </div>
+                    </div>
+                    
                 </div>
             </div>
         </nav>
