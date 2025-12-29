76e9363a4b7b8a5ed535c889c2196f96323bf2a6
Yash Panchal <panchalyash089@gmail.com>
2025-12-29 16:32:54 +0530

    added logo in navbar

--- foodies/src/Components/Menubar/Menubar.jsx
+++ foodies/src/Components/Menubar/Menubar.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './Menubar.css';
+import {assets} from '../../assets/assets';
 
 const Menubar = () => {
     return (
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">Navbar</a>
+                <img src={assets.logo} alt="Logo" className="logo" height={48} width={48} />
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
