07a28d4cdf75bf041df61cddd52fb09ccefd68a0
Yash Panchal <panchalyash089@gmail.com>
2025-12-30 15:10:27 +0530

    added images

--- foodies/src/Components/Menubar/ExploreMenu/ExploreMenu.jsx
+++ foodies/src/Components/Menubar/ExploreMenu/ExploreMenu.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import { categories } from '../../../assets/assets.js';
+import './ExploreMenu.css';
 
 const ExploreMenu = () => {
     return (
-        <div>
-            ExploreMenu
+        <div className="explore-menu position-relative">
+            <h1 className="d-flex align-items-center justify-content-between">
+                Explore Menu
+                <div className="d-flex">
+                    <i className='bi bi-arrow-left-circle scroll-icon'></i>
+                    <i className='bi bi-arrow-right-circle scroll-icon'></i>
+                </div>
+            </h1>
+            <p>Explore curated lists of dishes from top categories</p>
+            <div className="d-flex justify-content-between gap-4 overflow-auto explore-menu-list">
+                {
+                    categories.map((item, index) => {
+                        return (
+                            <div key={index} className="text-center explore-menu-list-item">
+                                <img src={item.icon} alt={item.category} className="rounded-circle" height={128} width={128} />
+                                <p className='mt-2 fw-bold'>{item.category}</p>
+                            </div>
+                        )
+                    })
+                }
+            </div>
         </div>
     )
 }
--- foodies/src/assets/assets.js
+++ foodies/src/assets/assets.js
@@ -1,7 +1,46 @@
 import logo from './logo.png';
 import cart from './cart.png';
 
+import biryani from './biryani.png';
+import burger from './burger.png';
+import cake from './cake.png';
+import icecream from './icecream.png';
+import pizza from './pizza.png';
+import rolls from './rolls.png';
+import salad from './salad.png';
+
 export const assets = {
     logo,
     cart
 }
+
+export const categories = [
+    {
+        category: 'Biryani',
+        icon: biryani
+    },
+    {
+        category: 'Burger',
+        icon: burger
+    },
+    {
+        category: 'Cakes',
+        icon: cake
+    },
+    {
+        category: 'Ice Cream',
+        icon: icecream
+    },
+    {
+        category: 'Pizza',
+        icon: pizza
+    },
+    {
+        category: 'Rolls',
+        icon: rolls
+    },
+    {
+        category: 'Salad',
+        icon: salad
+    }
+];
