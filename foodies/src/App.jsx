48488f45ba8c65b5ed00d86114fd16397a3aff14
Yash Panchal <panchalyash089@gmail.com>
2026-01-02 17:32:46 +0530

    adding FoodDetails

--- foodies/src/App.jsx
+++ foodies/src/App.jsx
@@ -4,6 +4,7 @@ import Menubar from './Components/Menubar/Menubar.jsx';
 import Home from './pages/Home/Home.jsx';
 import ExploreFood from './pages/ExploreFood/ExploreFood.jsx';
 import ContactUs from './pages/Contact/Contact.jsx';
+import FoodDetails from './pages/FoodDetails/FoodDetails.jsx';
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/explore" element={<ExploreFood />} />
         <Route path="/contact" element={<ContactUs />} />
+        <Route path="/food/:id" element={<FoodDetails />} />
       </Routes>
     </div>
   )
