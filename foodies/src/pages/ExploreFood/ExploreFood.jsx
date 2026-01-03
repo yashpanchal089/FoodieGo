e298fccf57759c1b92fabb2f7747804f1aa51bb0
Yash Panchal <panchalyash089@gmail.com>
2026-01-03 14:55:15 +0530

    Add  foods on explore page

--- foodies/src/pages/ExploreFood/ExploreFood.jsx
+++ foodies/src/pages/ExploreFood/ExploreFood.jsx
@@ -3,27 +3,30 @@ import FoodDisplay from '../../Components/Menubar/FoodDisplay/FoodDisplay';
 
 const ExploreFood = () => {
     return (
-        <div className="container">
-            <div className="row justify-content-center">
-                <div className="col-md-6">
-                    <div className="input-group mb-3">
-                        <select className='form-select mt-2' style={{'maxWidth': '150px'}}>
-                            <option value="Biryani">Biryani</option>
-                            <option value="Burger">Burger</option>
-                            <option value="Pizza">Pizza</option>
-                            <option value="Rolls">Rolls</option>
-                            <option value="Cakes">Cakes</option>
-                            <option value="Salad">Salad</option>
-                            <option value="Ice Cream">Ice Cream</option>
-                        </select>
-                        <input type="text" className='form-control mt-2' placeholder='Search your food' />
-                        <button className='btn btn-primary mt-2' type ="submit">
-                            <i className="bi bi-search"></i>
-                        </button>
+        <>
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div className="col-md-6">
+                        <div className="input-group mb-3">
+                            <select className='form-select mt-2' style={{ 'maxWidth': '150px' }}>
+                                <option value="Biryani">Biryani</option>
+                                <option value="Burger">Burger</option>
+                                <option value="Pizza">Pizza</option>
+                                <option value="Rolls">Rolls</option>
+                                <option value="Cakes">Cakes</option>
+                                <option value="Salad">Salad</option>
+                                <option value="Ice Cream">Ice Cream</option>
+                            </select>
+                            <input type="text" className='form-control mt-2' placeholder='Search your food' />
+                            <button className='btn btn-primary mt-2' type="submit">
+                                <i className="bi bi-search"></i>
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+            <FoodDisplay />
+        </>
     )
 }
 
