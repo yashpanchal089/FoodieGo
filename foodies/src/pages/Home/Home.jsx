10c24d46662b79cf36979db4f5bd0ab062254d23
Yash Panchal <panchalyash089@gmail.com>
2026-01-03 15:08:50 +0530

    importing the food display

--- foodies/src/pages/Home/Home.jsx
+++ foodies/src/pages/Home/Home.jsx
@@ -1,13 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../../Components/Menubar/Header/Header.jsx';
 import ExploreMenu from '../../Components/Menubar/ExploreMenu/ExploreMenu.jsx';
 import FoodDisplay from '../../Components/Menubar/FoodDisplay/FoodDisplay.jsx';
 
 const Home = () => {
+    const [category, setCategory] = useState('All');
     return (
         <main className='container'>
             <Header />
-            <ExploreMenu />
+            <ExploreMenu category={category} setCategory={setCategory} />
             <FoodDisplay />
         </main>
     )
