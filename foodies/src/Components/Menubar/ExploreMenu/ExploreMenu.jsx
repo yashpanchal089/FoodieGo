49095044ce7abe4593dbcdceae9383c552c9e944
Yash Panchal <panchalyash089@gmail.com>
2026-01-03 15:08:24 +0530

    Adding menu

--- foodies/src/Components/Menubar/ExploreMenu/ExploreMenu.jsx
+++ foodies/src/Components/Menubar/ExploreMenu/ExploreMenu.jsx
@@ -2,7 +2,7 @@ import React, { useRef } from 'react';
 import { categories } from '../../../assets/assets.js';
 import './ExploreMenu.css';
 
-const ExploreMenu = () => {
+const ExploreMenu = ({category, setCategory}) => {
 
     const menuref = useRef(null);
     const scrollLeft = () => {
@@ -31,8 +31,8 @@ const ExploreMenu = () => {
                 {
                     categories.map((item, index) => {
                         return (
-                            <div key={index} className="text-center explore-menu-list-item">
-                                <img src={item.icon} alt={item.category} className="rounded-circle" height={128} width={128} />
+                            <div key={index} className="text-center explore-menu-list-item" onClick={() => setCategory(prev => prev === item.category ? 'All' : item.category)}>
+                                <img src={item.icon} alt={item.category} className={item.category === category ? 'rounded-circle active' : 'rounded-circle'} height={128} width={128} />
                                 <p className='mt-2 fw-bold'>{item.category}</p>
                             </div>
                         )
