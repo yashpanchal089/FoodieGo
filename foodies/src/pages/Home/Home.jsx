332789e082717cfa0a43aeaf522f6d41ff8d71b8
Yash Panchal <panchalyash089@gmail.com>
2026-01-13 16:34:28 +0530

    adding search text option

--- foodies/src/pages/Home/Home.jsx
+++ foodies/src/pages/Home/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
         <main className='container'>
             <Header />
             <ExploreMenu category={category} setCategory={setCategory} />
-            <FoodDisplay category={category} />
+            <FoodDisplay category={category} searchText={''}/>
         </main>
     )
 }
