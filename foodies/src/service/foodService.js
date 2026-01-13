ac092b3b59e6c20216eb456fc7bf7cb25c0afbe9
Yash Panchal <panchalyash089@gmail.com>
2026-01-13 14:02:30 +0530

    changed the port

--- foodies/src/service/foodService.js
+++ foodies/src/service/foodService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080/api/foods';
+const API_URL = 'http://localhost:8081/api/foods';
 
 export const fetchfoodList = async () => {
     try {
