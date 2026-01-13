52783d5327fc91ed46ff2a7492766c662b081fe9
Yash Panchal <panchalyash089@gmail.com>
2026-01-13 17:27:37 +0530

    Clicked on logo navigate on home page

--- foodies/src/Components/Menubar/Menubar.jsx
+++ foodies/src/Components/Menubar/Menubar.jsx
@@ -7,7 +7,7 @@ const Menubar = () => {
     return (
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
             <div className="container">
-                <img src={assets.logo} alt="Logo" className="mx-2" height={48} width={48} />
+                <Link to="/"><img src={assets.logo} alt="Logo" className="mx-2" height={48} width={48} /></Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
