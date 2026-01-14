8e9d27432adf016cb5995cad592f577ba4d6bcad
Yash Panchal <panchalyash089@gmail.com>
2026-01-14 11:51:19 +0530

    Adding plus icon

--- foodies/src/Components/Menubar/FoodItem/FoodItem.jsx
+++ foodies/src/Components/Menubar/FoodItem/FoodItem.jsx
@@ -4,8 +4,10 @@ import { Link } from 'react-router-dom';
 const FoodItem = ({ name, description, id, imageUrl, price }) => {
     return (
         <div className="col-12 col-sm-6 col-md-4 col-lg-3 mb-4 d-flex justify-content-center">
-            <Link to={`/food/${id}`} className="card" style={{"maxWidth": "320px", "textDecoration": "none"}}>
-                <img src={imageUrl} className="card-img-top" alt="Product Image" height={300} width={60} />
+            <div className="card" style={{"maxWidth": "320px"}}>
+                <Link to={`/food/${id}`}>
+                    <img src={imageUrl} className="card-img-top" alt="Product Image" height={300} width={60} />
+                </Link>
                 <div className="card-body">
                     <h5 className="card-title">{name}</h5>
                     <p className="card-text">{description}</p>
@@ -23,9 +25,10 @@ const FoodItem = ({ name, description, id, imageUrl, price }) => {
                 </div>
                 <div className="card-footer d-flex justify-content-between bg-light">
                     <Link to={`/food/${id}`} className="btn btn-primary sm">View Food</Link>
-                    <button className="btn btn-outline-secondary btn-sm"><i className="bi bi-heart"></i></button>
+                    <button className="btn btn-primary btn-sm">
+                        <i className="bi bi-plus-circle"></i></button>
                 </div>
-            </Link>
+            </div>
         </div>
     )
 }
