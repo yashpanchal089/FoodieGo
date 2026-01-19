cb01ba7a8c30f77abadffae1115932fb1d87e584
Yash Panchal <panchalyash089@gmail.com>
2026-01-19 16:45:08 +0530

    High lighting the navlink word

--- foodies/src/Components/Menubar/Menubar.jsx
+++ foodies/src/Components/Menubar/Menubar.jsx
@@ -1,10 +1,11 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import './Menubar.css';
 import { assets } from '../../assets/assets';
 import { Link } from 'react-router-dom';
 import { StoreContext } from '../../context/StoreContext';
 
 const Menubar = () => {
+    const [active, setActive] = useState('home');
     const {quantities} = useContext(StoreContext);
     const uniqueItemInCart = Object.values(quantities).filter(qty => qty > 0).length;
     return (
@@ -17,13 +18,13 @@ const Menubar = () => {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <Link className="nav-link" to="/">Home</Link>
+                            <Link className={active === "home" ? "nav-link fw-bold" : "nav-link"} to="/" onClick={() => setActive('home')}>Home</Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/explore">Explore</Link>
+                            <Link className={active === "explore" ? "nav-link fw-bold" : "nav-link"} to="/explore" onClick={() => setActive('explore')}>Explore</Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/contact">Contact us</Link>
+                            <Link className={active === "contact-us" ? "nav-link fw-bold" : "nav-link"} to="/contact" onClick={() => setActive('contact-us')}>Contact us</Link>
                         </li>
                     </ul>
                     <div className="d-flex align-items-center gap-4">
