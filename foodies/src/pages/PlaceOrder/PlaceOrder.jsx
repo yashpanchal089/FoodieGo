0d0ea97bf0cab95469d533cbadf9957233e7100c
Yash Panchal <panchalyash089@gmail.com>
2026-01-28 15:05:44 +0530

    removed promo code

--- foodies/src/pages/PlaceOrder/PlaceOrder.jsx
+++ foodies/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -4,83 +4,7 @@ import "./PlaceOrder.css";
 const PlaceOrder = () => {
   return (
     <div className="container mt-4">
-      <div className="dropdown position-fixed bottom-0 end-0 mb-3 me-3 bd-mode-toggle">
-        {" "}
-        <button
-          className="btn btn-bd-primary py-2 dropdown-toggle d-flex align-items-center"
-          id="bd-theme"
-          type="button"
-          aria-expanded="false"
-          data-bs-toggle="dropdown"
-          aria-label="Toggle theme (auto)"
-        >
-          {" "}
-          <svg className="bi my-1 theme-icon-active" aria-hidden="true">
-            <use href="#circle-half"></use>
-          </svg>{" "}
-          <span className="visually-hidden" id="bd-theme-text">
-            Toggle theme
-          </span>{" "}
-        </button>{" "}
-        <ul
-          className="dropdown-menu dropdown-menu-end shadow"
-          aria-labelledby="bd-theme-text"
-        >
-          {" "}
-          <li>
-            {" "}
-            <button
-              type="button"
-              className="dropdown-item d-flex align-items-center"
-              data-bs-theme-value="light"
-              aria-pressed="false"
-            >
-              {" "}
-              <svg className="bi me-2 opacity-50" aria-hidden="true">
-                <use href="#sun-fill"></use>
-              </svg>
-              Light
-              <svg className="bi ms-auto d-none" aria-hidden="true">
-                <use href="#check2"></use>
-              </svg>{" "}
-            </button>{" "}
-          </li>{" "}
-          <li>
-            {" "}
-            <button
-              type="button"
-              className="dropdown-item d-flex align-items-center"
-              data-bs-theme-value="dark"
-              aria-pressed="false"
-            >
-              {" "}
-              <svg className="bi me-2 opacity-50" aria-hidden="true">
-                <use href="#moon-stars-fill"></use>
-              </svg>
-              Dark
-              <svg className="bi ms-auto d-none" aria-hidden="true">
-                <use href="#check2"></use>
-              </svg>{" "}
-            </button>{" "}
-          </li>{" "}
-          <li>
-            <button
-              type="button"
-              className="dropdown-item d-flex align-items-center active"
-              data-bs-theme-value="auto"
-              aria-pressed="true"
-            >
-              <svg className="bi me-2 opacity-50" aria-hidden="true">
-                <use href="#circle-half"></use>
-              </svg>
-              Auto
-              <svg className="bi ms-auto d-none" width="1em" height="1em">
-                <use href="#check2"></use>
-              </svg>
-            </button>
-          </li>
-        </ul>
-      </div>
+      
       <div className="container mt-4">
         <main>
           <div className="row g-5">
@@ -141,20 +65,7 @@ const PlaceOrder = () => {
                   <span>Total (USD)</span> <strong>$20</strong>{" "}
                 </li>{" "}
               </ul>{" "}
-              <form className="card p-2">
-                {" "}
-                <div className="input-group">
-                  {" "}
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Promo code"
-                  />{" "}
-                  <button type="submit" className="btn btn-secondary">
-                    Redeem
-                  </button>{" "}
-                </div>{" "}
-              </form>{" "}
+              
             </div>{" "}
             <div className="col-md-7 col-lg-8">
               {" "}
