3ab3bf8e12f5da7bce3d940eeb6f2a69e07bfb2d
Yash Panchal <panchalyash089@gmail.com>
2026-01-28 15:18:39 +0530

    Removed unnesccaery things

--- foodies/src/pages/PlaceOrder/PlaceOrder.jsx
+++ foodies/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -51,18 +51,10 @@ const PlaceOrder = () => {
                   </div>{" "}
                   <span className="text-body-secondary">$5</span>{" "}
                 </li>{" "}
-                <li className="list-group-item d-flex justify-content-between bg-body-tertiary">
-                  {" "}
-                  <div className="text-success">
-                    {" "}
-                    <h6 className="my-0">Promo code</h6>{" "}
-                    <small>EXAMPLECODE</small>{" "}
-                  </div>{" "}
-                  <span className="text-success">−$5</span>{" "}
-                </li>{" "}
+                
                 <li className="list-group-item d-flex justify-content-between">
                   {" "}
-                  <span>Total (USD)</span> <strong>$20</strong>{" "}
+                  <span>Total (INR)</span> <strong>&#8377;20</strong>{" "}
                 </li>{" "}
               </ul>{" "}
               
@@ -76,7 +68,7 @@ const PlaceOrder = () => {
                   {" "}
                   <div className="col-sm-6">
                     {" "}
-                    <label for="firstName" className="form-label">
+                    <label htmlFor="firstName" className="form-label">
                       First name
                     </label>{" "}
                     <input
@@ -87,13 +79,11 @@ const PlaceOrder = () => {
                       value=""
                       required
                     />{" "}
-                    <div className="invalid-feedback">
-                      Valid first name is required.
-                    </div>{" "}
+ 
                   </div>{" "}
                   <div className="col-sm-6">
                     {" "}
-                    <label for="lastName" className="form-label">
+                    <label htmlFor="lastName" className="form-label">
                       Last name
                     </label>{" "}
                     <input
@@ -104,14 +94,12 @@ const PlaceOrder = () => {
                       value=""
                       required
                     />{" "}
-                    <div className="invalid-feedback">
-                      Valid last name is required.
-                    </div>{" "}
+
                   </div>{" "}
                   <div className="col-12">
                     {" "}
-                    <label for="username" className="form-label">
-                      Username
+                    <label htmlFor="email" className="form-label">
+                      Email
                     </label>{" "}
                     <div className="input-group has-validation">
                       {" "}
@@ -119,34 +107,17 @@ const PlaceOrder = () => {
                       <input
                         type="text"
                         className="form-control"
-                        id="username"
-                        placeholder="Username"
+                        id="email"
+                        placeholder="Email"
                         required
                       />{" "}
-                      <div className="invalid-feedback">
-                        Your username is required.
-                      </div>{" "}
+
                     </div>{" "}
                   </div>{" "}
+                  
                   <div className="col-12">
                     {" "}
-                    <label for="email" className="form-label">
-                      Email{" "}
-                      <span className="text-body-secondary">(Optional)</span>
-                    </label>{" "}
-                    <input
-                      type="email"
-                      className="form-control"
-                      id="email"
-                      placeholder="you@example.com"
-                    />{" "}
-                    <div className="invalid-feedback">
-                      Please enter a valid email address for shipping updates.
-                    </div>{" "}
-                  </div>{" "}
-                  <div className="col-12">
-                    {" "}
-                    <label for="address" className="form-label">
+                    <label htmlFor="address" className="form-label">
                       Address
                     </label>{" "}
                     <input
@@ -156,13 +127,11 @@ const PlaceOrder = () => {
                       placeholder="1234 Main St"
                       required
                     />{" "}
-                    <div className="invalid-feedback">
-                      Please enter your shipping address.
-                    </div>{" "}
+
                   </div>{" "}
                   <div className="col-12">
                     {" "}
-                    <label for="address2" className="form-label">
+                    <label htmlFor="address2" className="form-label">
                       Address 2{" "}
                       <span className="text-body-secondary">(Optional)</span>
                     </label>{" "}
@@ -175,7 +144,7 @@ const PlaceOrder = () => {
                   </div>{" "}
                   <div className="col-md-5">
                     {" "}
-                    <label for="country" className="form-label">
+                    <label htmlFor="country" className="form-label">
                       Country
                     </label>{" "}
                     <select className="form-select" id="country" required>
@@ -183,13 +152,11 @@ const PlaceOrder = () => {
                       <option value="">Choose...</option>{" "}
                       <option>United States</option>{" "}
                     </select>{" "}
-                    <div className="invalid-feedback">
-                      Please select a valid country.
-                    </div>{" "}
+
                   </div>{" "}
                   <div className="col-md-4">
                     {" "}
-                    <label for="state" className="form-label">
+                    <label htmlFor="state" className="form-label">
                       State
                     </label>{" "}
                     <select className="form-select" id="state" required>
@@ -197,13 +164,11 @@ const PlaceOrder = () => {
                       <option value="">Choose...</option>{" "}
                       <option>California</option>{" "}
                     </select>{" "}
-                    <div className="invalid-feedback">
-                      Please provide a valid state.
-                    </div>{" "}
+
                   </div>{" "}
                   <div className="col-md-3">
                     {" "}
-                    <label for="zip" className="form-label">
+                    <label htmlFor="zip" className="form-label">
                       Zip
                     </label>{" "}
                     <input
@@ -213,9 +178,7 @@ const PlaceOrder = () => {
                       placeholder=""
                       required
                     />{" "}
-                    <div className="invalid-feedback">
-                      Zip code required.
-                    </div>{" "}
+
                   </div>{" "}
                 </div>{" "}
                 <hr className="my-4" />{" "}
