27ab26ba3932dfa0168776b9a128ae424ae4b48e
Yash Panchal <panchalyash089@gmail.com>
2026-02-06 14:59:55 +0530

    Adding Login form

--- foodies/src/Components/Menubar/Login/Login.jsx
+++ foodies/src/Components/Menubar/Login/Login.jsx
@@ -4,8 +4,9 @@ import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
-    <div className="container login-container">
-      <div className="row">
+    <div className="login-container">
+      <div className="container">
+        <div className="row">
         <div className="col-sm-9 col-md-7 col-lg-5 mx-auto">
           <div className="card border-0 shadow rounded-3 my-5">
             <div className="card-body p-4 p-sm-5">
@@ -51,6 +52,7 @@ const Login = () => {
               </form>
             </div>
           </div>
+        </div>
         </div>
       </div>
     </div>
