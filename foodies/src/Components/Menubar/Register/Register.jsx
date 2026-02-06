263e49eb0f2c527d780c51efaad7f9d4578ac999
Yash Panchal <panchalyash089@gmail.com>
2026-02-06 13:23:34 +0530

    Adding Rester Form

--- foodies/src/Components/Menubar/Register/Register.jsx
+++ foodies/src/Components/Menubar/Register/Register.jsx
@@ -1,10 +1,59 @@
 import React from "react";
-import './Register.css';
+import "./Register.css";
 
 const Register = () => {
-  return <div>
-    Register
-  </div>;
+  return (
+    <div className="container login-container">
+      <div className="row">
+        <div className="col-sm-9 col-md-7 col-lg-5 mx-auto">
+          <div className="card border-0 shadow rounded-3 my-5">
+            <div className="card-body p-4 p-sm-5">
+              <h5 className="card-title text-center mb-5 fw-light fs-5">
+                Sign In
+              </h5>
+              <form onSubmit={(e) => e.preventDefault()}>
+                <div className="form-floating mb-3">
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="floatingInput"
+                    placeholder="name@example.com"
+                  />
+                  <label htmlFor="floatingInput">Email address</label>
+                </div>
+                <div className="form-floating mb-3">
+                  <input
+                    type="password"
+                    className="form-control"
+                    id="floatingPassword"
+                    placeholder="Password"
+                  />
+                  <label htmlFor="floatingPassword">Password</label>
+                </div>
+                <div className="d-grid">
+                  <button
+                    className="btn btn-outline-primary btn-login text-uppercase"
+                    type="submit"
+                  >
+                    Sign in
+                  </button>
+                  <button
+                    className="btn btn-outline-danger btn-login text-uppercase mt-2"
+                    type="reset"
+                  >
+                    Reset
+                  </button>
+                </div>
+                <div className="mt-4">
+                  Already have an account? <Link to="/register">Sign Up</Link>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
