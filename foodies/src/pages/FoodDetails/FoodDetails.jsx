404b20060c5d373cfb9916deea13e6c9e54632e1
Yash Panchal <panchalyash089@gmail.com>
2026-02-06 12:05:18 +0530

    Adding Food in cart from image

--- foodies/src/pages/FoodDetails/FoodDetails.jsx
+++ foodies/src/pages/FoodDetails/FoodDetails.jsx
@@ -1,11 +1,14 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom'
+import React, { useContext, useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom'
 import { fetchFoodDetails } from '../../service/foodService';
 import { toast } from 'react-toastify';
+import { StoreContext } from '../../context/StoreContext.jsx';
 
 const FoodDetails = () => {
     const { id } = useParams();
+    const { increaseQty } = useContext(StoreContext);
+    const navigate = useNavigate();
 
     const [data, setData] = useState({});
 
@@ -20,6 +23,11 @@ const FoodDetails = () => {
         }
         loadFoodDetails();
     }, [id]);
+
+    const addToCart = () => {
+        increaseQty(data.id);
+        navigate("/cart");
+    }
     return (
         <section className="py-5">
             <div className="container px-4 px-lg-5 my-5">
@@ -33,7 +41,7 @@ const FoodDetails = () => {
                         </div>
                         <p className="lead">{data.description}</p>
                         <div className="d-flex">
-                            <button className="btn btn-outline-dark flex-shrink-0" type="button">
+                            <button className="btn btn-outline-dark flex-shrink-0" type="button" onClick={addToCart}>
                                 <i className="bi-cart-fill me-1"></i>
                                 Add to cart
                             </button>
