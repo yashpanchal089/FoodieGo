486c5a4bc0573e5c1238d37cf08a10a59f2b8b6a
Yash Panchal <panchalyash089@gmail.com>
2026-02-06 15:00:12 +0530

    Adding Register Form

--- foodies/src/Components/Menubar/Register/Register.jsx
+++ foodies/src/Components/Menubar/Register/Register.jsx
@@ -4,8 +4,9 @@ import { Link } from "react-router-dom";
 
 const Register = () => {
   return (
-    <div className="container login-container">
-      <div className="row">
+    <div className="register-container">
+      <div className="container">
+        <div className="row">
         <div className="col-sm-9 col-md-7 col-lg-5 mx-auto">
           <div className="card border-0 shadow rounded-3 my-5">
             <div className="card-body p-4 p-sm-5">
@@ -60,6 +61,7 @@ const Register = () => {
               </form>
             </div>
           </div>
+        </div>
         </div>
       </div>
     </div>
