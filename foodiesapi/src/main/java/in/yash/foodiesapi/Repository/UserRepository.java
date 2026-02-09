8120e105f7c645ae02256f0fa57c0182f7c6f350
Yash Panchal <panchalyash089@gmail.com>
2026-02-09 17:27:07 +0530

    Create new file

---
+++ foodiesapi/src/main/java/in/yash/foodiesapi/Repository/UserRepository.java
@@ -0,0 +1,12 @@
+package in.yash.foodiesapi.Repository;
+
+import in.yash.foodiesapi.io.UserEntity;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends MongoRepository<UserEntity, String> {
+    Optional<UserEntity> findByEmail(String email);
+}
