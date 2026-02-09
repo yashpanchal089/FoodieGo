929ef39d45734e7923dac2c143c5374f944724da
Yash Panchal <panchalyash089@gmail.com>
2026-02-09 17:26:45 +0530

    Create new file

---
+++ foodiesapi/src/main/java/in/yash/foodiesapi/io/UserRequest.java
@@ -0,0 +1,17 @@
+package in.yash.foodiesapi.io;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserRequest {
+    private String name;
+    private String email;
+    private String password;
+
+}
