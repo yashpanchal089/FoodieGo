9587bb00c6619d17a6f8da4d7da41fd3c98058ec
Yash Panchal <panchalyash089@gmail.com>
2026-02-09 17:27:33 +0530

    Create new file

---
+++ foodiesapi/src/main/java/in/yash/foodiesapi/Service/UserService.java
@@ -0,0 +1,8 @@
+package in.yash.foodiesapi.Service;
+
+import in.yash.foodiesapi.io.UserRequest;
+import in.yash.foodiesapi.io.UserResponse;
+
+public interface UserService {
+    UserResponse registerUser(UserRequest request);
+}
