35b6ebfe98a9ed5f5e5766dfa36ea27d32eac805
Yash Panchal <panchalyash089@gmail.com>
2026-02-09 17:27:27 +0530

    Added UserDetails Service

---
+++ foodiesapi/src/main/java/in/yash/foodiesapi/Service/AppUserDetailsService.java
@@ -0,0 +1,32 @@
+package in.yash.foodiesapi.Service;
+
+import in.yash.foodiesapi.Repository.UserRepository;
+import in.yash.foodiesapi.io.UserEntity;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AppUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email)
+            throws UsernameNotFoundException {
+
+        UserEntity user = userRepository.findByEmail(email)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User not found"));
+
+        return User.builder()
+                .username(user.getEmail())
+                .password(user.getPassword()) // BCrypt password
+                .roles("USER")
+                .build();
+    }
+}
