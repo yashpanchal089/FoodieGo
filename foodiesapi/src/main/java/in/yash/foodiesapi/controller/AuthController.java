a96583da13c96566a856ca2a34df585f95826b95
Yash Panchal <panchalyash089@gmail.com>
2026-02-09 17:25:13 +0530

    Added authtincation

---
+++ foodiesapi/src/main/java/in/yash/foodiesapi/controller/AuthController.java
@@ -0,0 +1,46 @@
+package in.yash.foodiesapi.controller;
+
+import in.yash.foodiesapi.Service.AppUserDetailsService;
+import in.yash.foodiesapi.io.AuthenticationRequest;
+import in.yash.foodiesapi.io.AutheticationResponse;
+import in.yash.foodiesapi.util.JwtUtil;
+import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@AllArgsConstructor
+public class AuthController {
+
+    private final AuthenticationManager authenticationManager;
+    private final AppUserDetailsService userDetailsService;
+    private final JwtUtil jwtUtil;
+
+    @PostMapping("/login")
+    public AutheticationResponse login(
+            @RequestBody AuthenticationRequest request) {
+
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+
+        UserDetails userDetails =
+                userDetailsService.loadUserByUsername(request.getEmail());
+
+        String jwtToken = jwtUtil.generateToken(userDetails);
+
+        return new AutheticationResponse(
+                request.getEmail(),
+                jwtToken
+        );
+    }
+}
