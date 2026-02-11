cc3bdf35801700a7ab03266b5bf0b04208cf3400
Yash Panchal <panchalyash089@gmail.com>
2026-02-11 10:58:24 +0530

    Printing added food on cart

--- foodiesapi/src/main/java/in/yash/foodiesapi/controller/CartController.java
+++ foodiesapi/src/main/java/in/yash/foodiesapi/controller/CartController.java
@@ -1,12 +1,16 @@
 package in.yash.foodiesapi.controller;
 
 import in.yash.foodiesapi.Service.CartService;
+import in.yash.foodiesapi.io.CartRequest;
+import in.yash.foodiesapi.io.CartResponse;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Map;
 
@@ -17,12 +21,12 @@ public class CartController {
 
     private final CartService cartService;
     @PostMapping
-    public ResponseEntity<?> addToCart(@RequestBody Map<String, String> request){
-        String foodId = request.get("foodId");
+    public CartResponse addToCart(@RequestBody CartRequest request){
+        String foodId = request.getFoodId();
         if(foodId == null || foodId.isEmpty()){
-            return ResponseEntity.badRequest().body("Food Id is required");
+            throw  new ResponseStatusException(HttpStatus.BAD_REQUEST, "Foodid not found");
         }
-        cartService.addToCart(foodId);
-        return ResponseEntity.ok().body(null);
+        return cartService.addToCart(request);
+
     }
 }
