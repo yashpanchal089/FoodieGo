34658ff951c7cb5becd7ba829e2be3abfb7595ef
Yash Panchal <panchalyash089@gmail.com>
2026-02-11 16:18:21 +0530

    clar cart

--- foodiesapi/src/main/java/in/yash/foodiesapi/controller/CartController.java
+++ foodiesapi/src/main/java/in/yash/foodiesapi/controller/CartController.java
@@ -6,10 +6,7 @@ import in.yash.foodiesapi.io.CartResponse;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Map;
@@ -27,6 +24,16 @@ public class CartController {
             throw  new ResponseStatusException(HttpStatus.BAD_REQUEST, "Foodid not found");
         }
         return cartService.addToCart(request);
+    }
 
+    @GetMapping
+    public CartResponse getCart() {
+        return cartService.getCart();
+    }
+
+    @DeleteMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void clearCart(){
+        cartService.clearCart();
     }
 }
