1294f8bac09e906a8a6b7b2d21582252bc761430
Yash Panchal <panchalyash089@gmail.com>
2026-02-11 10:34:11 +0530

    New service file with implementation

---
+++ foodiesapi/src/main/java/in/yash/foodiesapi/Service/CartServiceImpl.java
@@ -0,0 +1,29 @@
+package in.yash.foodiesapi.Service;
+
+import in.yash.foodiesapi.Repository.CartRepository;
+import in.yash.foodiesapi.entity.CartEntity;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class CartServiceImpl implements CartService{
+
+    private final CartRepository cartRepository;
+    private final UserService userService;
+
+    @Override
+    public void addToCart(String foodId) {
+        String loggedInUserId = userService.findByUserId();
+        Optional<CartEntity> cartOptional =  cartRepository.findByUserId(loggedInUserId);
+        CartEntity cart = cartOptional.orElseGet(() -> new CartEntity(loggedInUserId, new HashMap<>()));
+        Map<String, Integer> cartItems = cart.getItems();
+        cartItems.put(foodId, cartItems.getOrDefault(foodId, 0) + 1);
+        cart.setItems(cartItems);
+        cart = cartRepository.save(cart);
+    }
+}
