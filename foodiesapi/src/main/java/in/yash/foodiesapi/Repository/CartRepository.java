5e9adfe3cec4563c1e2c2ad99b2b640a99dc191e
Yash Panchal <panchalyash089@gmail.com>
2026-02-11 10:33:00 +0530

    Created new repository file

---
+++ foodiesapi/src/main/java/in/yash/foodiesapi/Repository/CartRepository.java
@@ -0,0 +1,16 @@
+package in.yash.foodiesapi.Repository;
+
+import in.yash.foodiesapi.entity.CartEntity;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CartRepository extends MongoRepository<CartEntity, String>{
+
+    Optional<CartEntity> findByUserId(String userId);
+
+    Void deleteByUserId(String userId);
+
+}
