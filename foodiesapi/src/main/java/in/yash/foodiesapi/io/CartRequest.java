25613629911e5141a3b9bd7ebfae62aecc5cf5be
Yash Panchal <panchalyash089@gmail.com>
2026-02-11 10:32:30 +0530

    Created new file

---
+++ foodiesapi/src/main/java/in/yash/foodiesapi/io/CartRequest.java
@@ -0,0 +1,19 @@
+package in.yash.foodiesapi.io;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CartRequest {
+    private String userId;
+    private Map<String, Integer> items = new HashMap<>();
+
+}
