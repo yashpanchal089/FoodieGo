f4e045ce8da0f05d9719ea3492e691b5c4066448
Yash Panchal <panchalyash089@gmail.com>
2026-02-11 10:59:09 +0530

    Adding food in cart

--- foodiesapi/src/main/java/in/yash/foodiesapi/Service/CartService.java
+++ foodiesapi/src/main/java/in/yash/foodiesapi/Service/CartService.java
@@ -1,6 +1,9 @@
 package in.yash.foodiesapi.Service;
 
+import in.yash.foodiesapi.io.CartRequest;
+import in.yash.foodiesapi.io.CartResponse;
+
 public interface CartService {
 
-    void addToCart(String foodId);
+    CartResponse addToCart(CartRequest request);
 }
