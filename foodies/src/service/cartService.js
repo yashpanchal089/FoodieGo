73ba404bbd1a3016b124858aca5b29eb80566dd6
Yash Panchal <panchalyash089@gmail.com>
2026-02-12 17:24:41 +0530

    New File created

---
+++ foodies/src/service/cartService.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:8081/api/cart";
+
+export const addToCart = async (foodId, token) => {
+    try {
+        await axios.post(API_URL,
+            { foodId },
+            { headers: { Authorization: `Bearer ${token}` } });
+
+    } catch (error) {
+        console.error("Failed to add item to cart", error);
+    }
+}
+
+export const removeQtyFromCart = async (foodId, token) => {
+    try {
+        await axios.post(API_URL + "/remove", { foodId }, { headers: { Authorization: `Bearer ${token}` } });
+
+    } catch (error) {
+        console.error("Failed to remove item from cart", error);
+    }
+}
+
+export const getCartData = async (token) => {
+    try {
+        const response = await axios.get(API_URL, { headers: { Authorization: `Bearer ${token}` } });
+        return response.data.items;
+    } catch (error) {
+        console.error("Failed to load cart data", error);
+    }
+}
